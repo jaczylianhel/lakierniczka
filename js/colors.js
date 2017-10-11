eb1ab473b5e84b70d8fc51cf3f3dfd9b8f9a37af
Aniol <jaczylianhel@gmail.com>
2017-10-11 20:49:01 +0200

    lakierniczka

---
+++ js/colors.js
@@ -0,0 +1,62 @@
+var colors = [
+  {
+	label: 'cyan',
+	value: 'CYA'
+  },
+  {
+	label: 'czerwony',
+	value: 'CZR'
+  },
+  {
+	label: 'zielony',
+	value: 'ZIE'
+  },
+  {
+	label: 'niebieski',
+	value: 'NIE'
+  },
+  {
+	label: 'pomaranczowy',
+	value: 'POM'
+  },
+  {
+	label: 'zolty',
+	value: 'zol'
+  },
+  {
+	label: 'lapiz',
+	value: 'LAP'
+  },
+  {
+	label: 'bialy',
+	value: 'BIA'
+  },
+  {
+	label: 'czarny',
+	value: 'CZA'
+  },
+   {
+	label: 'rozowy',
+	value: 'Roz'
+  },
+  {
+	label: 'kasztanowy',
+	value: 'KAS'
+  },
+  {
+	label: 'fiolet',
+	value: 'FIO'
+  },
+  {
+	label: 'mietowy',
+	value: 'MIE'
+  },
+  {
+	label: 'limonka',
+	value: 'LIM'
+  },
+  {
+	label: 'bezowy',
+	value: 'BEZ'
+  }
+];
